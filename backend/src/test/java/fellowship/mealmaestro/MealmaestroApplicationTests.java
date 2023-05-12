d16963dcdfef9e40f63c9dc668cb37e694dfd593
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-05-12 21:24:27 +0200

    🔧 Added Backend Files

---
+++ backend/src/test/java/fellowship/mealmaestro/MealmaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package fellowship.mealmaestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmaestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
