7054c5f166ce023bd8489b610bb57ba41f0b3cf0
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-05-22 21:55:18 +0200

    🚑 Quick test fix

--- frontend/src/app/pages/shopping/shopping.page.spec.ts
+++ frontend/src/app/pages/shopping/shopping.page.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShoppingPage } from './shopping.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('ShoppingPage', () => {
   let component: ShoppingPage;
   let fixture: ComponentFixture<ShoppingPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [ShoppingPage, IonicModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ShoppingPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
