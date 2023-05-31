bc7ed675e225428bfd0639ac721ea15ef1870260
AmickeC <croucampa01@gmail.com>
2023-06-01 00:44:32 +0200

    navbar removed

--- frontend/src/app/pages/tabs/tabs.routes.ts
+++ frontend/src/app/pages/tabs/tabs.routes.ts
@@ -21,11 +21,11 @@ export const routes: Routes = [
         loadComponent: () =>
           import('../profile/profile.page').then((m) => m.ProfilePage),
       },
-      {
-        path: 'signup',
-        loadComponent: () =>
-          import('../signup/signup.page').then((m) => m.SignupPage),
-      },
+      // {
+      //   path: 'signup',
+      //   loadComponent: () =>
+      //     import('../signup/signup.page').then((m) => m.SignupPage),
+      // },
       {
         path: '',
         redirectTo: '/tabs/home',
