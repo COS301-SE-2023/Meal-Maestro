d5656d7760b867e8f2e89a9fc8917660a2413cc7
AmickeC <croucampa01@gmail.com>
2023-05-31 18:43:14 +0200

    created a signup page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -17,5 +17,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
   },
+
 ];
---
+++ frontend/src/app/pages/signup/signup.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.page.html',
+  styleUrls: ['./signup.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class SignupPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
