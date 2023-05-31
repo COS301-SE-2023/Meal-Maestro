511e24d39f9efcfc8b4e398274ebc22c7347d835
AmickeC <croucampa01@gmail.com>
2023-06-01 01:27:27 +0200

    navbar added

--- frontend/src/app/pages/browse/browse.page.ts
+++ frontend/src/app/pages/browse/browse.page.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponent } from '../../components/explore-container/explore-container.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.page.html',
   styleUrls: ['./browse.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, ExploreContainerComponent]
 })
-export class BrowsePage implements OnInit {
+export class BrowsePage {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
