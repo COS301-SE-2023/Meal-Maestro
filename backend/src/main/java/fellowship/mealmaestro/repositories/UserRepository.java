f9d79dc322f99f8dcb5c02aebe757da9947cf561
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-06-14 00:24:21 +0200

    🗃️ Worked on User Endpoint

--- backend/src/main/java/fellowship/mealmaestro/repositories/UserRepository.java
+++ backend/src/main/java/fellowship/mealmaestro/repositories/UserRepository.java
@@ -7,6 +7,8 @@ import org.neo4j.driver.Values;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import fellowship.mealmaestro.models.UserModel;
+
 @Repository
 public class UserRepository {
     
@@ -18,34 +20,35 @@ public class UserRepository {
     }
 
     //#region Create
-    public void createUser(String username, String password){
+    public void createUser(UserModel user){
         try (Session session = driver.session()){
-            session.executeWrite(createUserTransaction(username, password));
+
+            session.executeWrite(createUserTransaction(user.getUsername(), user.getPassword(), user.getEmail()));
         }
     }
 
-    public static TransactionCallback<Void> createUserTransaction(String username, String password) {
+    public static TransactionCallback<Void> createUserTransaction(String username, String password, String email) {
         //creates user with default pantry, shopping list, recipe book, and preferences
         return transaction -> {
-            transaction.run("CREATE (:Preferences)<-[:HAS_PREFERENCES]-(n0:User {name: $username, password: $password})-[:HAS_PANTRY]->(:Pantry {capacity: 0}),\r\n" + //
+            transaction.run("CREATE (:Preferences)<-[:HAS_PREFERENCES]-(n0:User {username: $username, password: $password, email: $email})-[:HAS_PANTRY]->(:Pantry {capacity: 0}),\r\n" + //
                     "(:`Shopping List`)<-[:HAS_LIST]-(n0)-[:HAS_RECIPE_BOOK]->(:`Recipe Book`)",
-            Values.parameters("name", username, "password", password));
+            Values.parameters("username", username, "password", password, "email", email));
             return null;
         };
     }
     //#endregion
 
     //#region Check User
-    public boolean checkUser(String username, String password){
+    public boolean checkUser(UserModel user){
         try (Session session = driver.session()){
-            return session.executeRead(checkUserTransaction(username, password));
+            return session.executeRead(checkUserTransaction(user.getUsername(), user.getEmail()));
         }
     }
 
-    public static TransactionCallback<Boolean> checkUserTransaction(String username, String password) {
+    public static TransactionCallback<Boolean> checkUserTransaction(String username, String email) {
         return transaction -> {
-            var result = transaction.run("MATCH (n0:User {name: $username, password: $password}) RETURN n0",
-            Values.parameters("name", username, "password", password));
+            var result = transaction.run("MATCH (n0:User {username: $username, email: $email}) RETURN n0",
+            Values.parameters("username", username, "email", email));
             return result.hasNext();
         };
     }
