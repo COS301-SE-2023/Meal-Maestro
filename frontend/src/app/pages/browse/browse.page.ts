19592edc540ca12ab7c9992afb128b9f1e554e8a
AmickeC <croucampa01@gmail.com>
2023-06-22 21:32:59 +0200

    scroll added

--- frontend/src/app/pages/browse/browse.page.ts
+++ frontend/src/app/pages/browse/browse.page.ts
@@ -21,6 +21,7 @@ export class BrowsePage {
     { title: "Fried Chicken Tenders", description: "Crispy and golden-brown on the outside, tender and juicy on the inside, the classic comfort food favorite", url: "https://images.unsplash.com/photo-1614398751058-eb2e0bf63e53?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1014&q=80" },
     { title: "Spaghetti & Prawns", description: "This Italian-inspired recipe is a crowd-pleaser and can be made in advance, making it perfect for meal prepping.", url: "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" },
     // Additional entries...
+    
   ];
   
 
