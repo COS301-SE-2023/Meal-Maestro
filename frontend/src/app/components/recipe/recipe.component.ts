c0e05b084aa0a2432db68cf21fd6729c735141f5
AmickeC <croucampa01@gmail.com>
2023-06-22 19:37:33 +0200

    component with data populated

--- frontend/src/app/components/recipe/recipe.component.ts
+++ frontend/src/app/components/recipe/recipe.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../../components/explore-container/explore-container.component';
 import { Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [CommonModule, IonicModule, ExploreContainerComponent]
 })
-export class RecipeComponent  implements OnInit {
+export class RecipeComponent {
 
-  constructor() { }
+  @Input() arrayData!: { title: string, description: string, url: string }[];
+  constructor() { 
+
+  }
 
   ngOnInit() {}
 
