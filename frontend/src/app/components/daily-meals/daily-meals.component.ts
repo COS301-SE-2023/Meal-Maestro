90124406b7a9ce5b4c0d5a6e3500c8481a999bb4
AmickeC <croucampa01@gmail.com>
2023-06-23 14:16:39 +0200

    array made & css updated

--- frontend/src/app/components/daily-meals/daily-meals.component.ts
+++ frontend/src/app/components/daily-meals/daily-meals.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -11,8 +11,19 @@ import { IonicModule } from '@ionic/angular';
 })
 export class DailyMealsComponent  implements OnInit {
 
+  @Input() mealsData!: { title: string, description: string, url: string }[];
+  isModalOpen = false;
+  currentObject :any
+  setOpen(isOpen: boolean, o :any) {
+    this.isModalOpen = isOpen;
+    this.setCurrent(o)
+  }
   constructor() { }
 
   ngOnInit() {}
+
+  setCurrent(o : any) {
+    this.currentObject = o;
+  }
 
 }
