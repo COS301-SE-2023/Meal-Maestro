136f336b6e18f4773a6f689c8b3a549174959096
skitsbi <u04525087@tuks.co.za>
2023-06-23 12:56:35 +0200

    Create openaiApiService.java

---
+++ backend/src/main/java/fellowship/mealmaestro/services/openaiApiService.java
@@ -0,0 +1,12 @@
+package fellowship.mealmaestro.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class openaiApiService {
+    
+}
