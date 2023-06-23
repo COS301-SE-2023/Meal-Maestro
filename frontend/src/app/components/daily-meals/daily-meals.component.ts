3b9a6d7b8d7c5da394fe1950de0113ad58fb60ec
AmickeC <croucampa01@gmail.com>
2023-06-23 14:05:26 +0200

    card content created

--- frontend/src/app/components/daily-meals/daily-meals.component.ts
+++ frontend/src/app/components/daily-meals/daily-meals.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-daily-meals',
   templateUrl: './daily-meals.component.html',
   styleUrls: ['./daily-meals.component.scss'],
+  standalone : true,
+  imports: [CommonModule, IonicModule],
 })
 export class DailyMealsComponent  implements OnInit {
 
--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { DailyMealsComponent } from '../../components/daily-meals/daily-meals.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, DailyMealsComponent],
 })
 export class HomePage {
   constructor() {}
