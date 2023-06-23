a500529fce8b084c3baa2b84577e47b01e3d2dd6
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-06-23 12:02:23 +0200

    fixed test

--- frontend/src/app/pages/recipe-book/recipe-book.page.spec.ts
+++ frontend/src/app/pages/recipe-book/recipe-book.page.spec.ts
@@ -5,11 +5,14 @@ describe('RecipeBookPage', () => {
   let component: RecipeBookPage;
   let fixture: ComponentFixture<RecipeBookPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [RecipeBookPage],
+    }).compileComponents();
     fixture = TestBed.createComponent(RecipeBookPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
