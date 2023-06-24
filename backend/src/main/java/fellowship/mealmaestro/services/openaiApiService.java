c2ee39db3c35629ff0cdde323354bf9c9614a823
skitsbi <u04525087@tuks.co.za>
2023-06-24 10:38:10 +0200

    cleaned up a bit

--- backend/src/main/java/fellowship/mealmaestro/services/openaiApiService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/openaiApiService.java
@@ -1,8 +1,5 @@
 package fellowship.mealmaestro.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
