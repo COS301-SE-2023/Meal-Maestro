c56249dfc93a675e3ada561b803ce9f733999130
skitsbi <u04525087@tuks.co.za>
2023-06-24 10:37:22 +0200

    ctrl+f

--- backend/src/main/java/fellowship/mealmaestro/services/openaiApiService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/openaiApiService.java
@@ -26,94 +26,92 @@ public class openaiApiService {
 
     private int maximumTokenLength = 300;
 
-    //potential vars
-    //will make a few prompts and return best, heavy on token use
+    // potential vars
+    // will make a few prompts and return best, heavy on token use
     private int bestOfN = 1;
-    //detect abuse
+    // detect abuse
     private String user = "";
-    //echo back prompt and its compeletion
+    // echo back prompt and its compeletion
     private boolean echo = false;
-    //stream prompt as it generates
+    // stream prompt as it generates
     private boolean stream = false;
 
-
-    //puts together prompt from common best practises
+    // puts together prompt from common best practises
     public String buildPrompt(String context, String goal, String format, String secondaryTasks, String examples) {
-        String Prompt = context + "\r\n\r\n" + goal + "\r\n\r\n" + format + "\r\n\r\n" + secondaryTasks + "\r\n\r\n" + examples;
-       
+        String Prompt = context + "\r\n\r\n" + goal + "\r\n\r\n" + format + "\r\n\r\n" + secondaryTasks + "\r\n\r\n"
+                + examples;
+
         return Prompt;
     }
 
     // builds the json styles api request string /////////////////
-    public String buildJsonApiRequest(){
+    public String buildJsonApiRequest() {
 
         String prompt = buildPrompt("", "", "", "", "");
 
-        String jsonRequest = "{" 
+        String jsonRequest = "{"
 
-        + "\"" + "model" + "\":" + "\"" + model + "\","
-        + "\"" + "prompt" + "\":" + "\"" + prompt + "\","
+                + "\"" + "model" + "\":" + "\"" + model + "\","
+                + "\"" + "prompt" + "\":" + "\"" + prompt + "\","
 
-        + "\"" + "temperature" + "\":" + temperature + ","
-        + "\"" + "max_tokens" + "\":" + maximumTokenLength + ","
-        + "\"" + "top_p" + "\":" + topP + ","
+                + "\"" + "temperature" + "\":" + temperature + ","
+                + "\"" + "max_tokens" + "\":" + maximumTokenLength + ","
+                + "\"" + "top_p" + "\":" + topP + ","
 
-        + "\"" + "frequency_penalty" + "\":"  + freqPenalty + ","
-        + "\"" + "presence_penalty" + "\":"  + presencePenalty + ","
+                + "\"" + "frequency_penalty" + "\":" + freqPenalty + ","
+                + "\"" + "presence_penalty" + "\":" + presencePenalty + ","
 
-        + "\"" + "n" + "\":"  + bestOfN + ""
+                + "\"" + "n" + "\":" + bestOfN + ""
 
-        + "}";
-        
+                + "}";
+
         return jsonRequest;
     }
+
     // build on predetermined prompt
-    public String buildJsonApiRequest(String prompt){
-        String jsonRequest = "{" 
+    public String buildJsonApiRequest(String prompt) {
+        String jsonRequest = "{"
 
-        + "\"" + "model" + "\":" + "\"" + model + "\","
-        + "\"" + "prompt" + "\":" + "\"" + prompt + "\","
-        // doubles and ints dont get qouted
-        + "\"" + "temperature" + "\":"  + temperature + ","
-        + "\"" + "max_tokens" + "\":"  + maximumTokenLength + ","
-        + "\"" + "top_p" + "\":"  + topP + ","
+                + "\"" + "model" + "\":" + "\"" + model + "\","
+                + "\"" + "prompt" + "\":" + "\"" + prompt + "\","
+                // doubles and ints dont get qouted
+                + "\"" + "temperature" + "\":" + temperature + ","
+                + "\"" + "max_tokens" + "\":" + maximumTokenLength + ","
+                + "\"" + "top_p" + "\":" + topP + ","
 
-        + "\"" + "frequency_penalty" + "\":" + freqPenalty + ","
-        + "\"" + "presence_penalty" + "\":" + presencePenalty 
+                + "\"" + "frequency_penalty" + "\":" + freqPenalty + ","
+                + "\"" + "presence_penalty" + "\":" + presencePenalty
 
-        + "}";
-        
+                + "}";
+
         return jsonRequest;
     }
     ///////////////////////////////////////////////////////////
 
     // setters ////////////////////////////////////////////////
-    public void setModel(String model){
+    public void setModel(String model) {
         this.model = model;
     }
 
-    public void setStop(String Stop){
+    public void setStop(String Stop) {
         this.stop = Stop;
     }
 
-    public void setTemperature(double x){
+    public void setTemperature(double x) {
         this.temperature = x;
     }
 
-    public void setTopP(double x){
+    public void setTopP(double x) {
         this.topP = x;
     }
 
-    public void setfreqPenalty(double x){
+    public void setfreqPenalty(double x) {
         this.temperature = x;
     }
 
-    public void setPresencePenalty(double x){
+    public void setPresencePenalty(double x) {
         this.temperature = x;
     }
     //////////////////////////////////////////////////////////////
-
-
-
 
 }
