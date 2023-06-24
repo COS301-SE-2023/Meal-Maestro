d78b71cf997ca79cc9a5fa22f9dfd97877cb3010
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-06-24 14:10:37 +0200

    ✨ Pantry page functionality completed

--- frontend/src/app/components/food-list-item/food-list-item.component.ts
+++ frontend/src/app/components/food-list-item/food-list-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { ActionSheetController, IonItemSliding, IonicModule } from '@ionic/angular';
+import { ActionSheetController, IonItemSliding, IonicModule, PickerController } from '@ionic/angular';
 import { FoodItemI } from '../../models/interfaces.model';
 import { PantryApiService } from '../../services/pantry-api/pantry-api.service';
 
@@ -15,7 +15,7 @@ export class FoodListItemComponent  implements OnInit {
   @Output() itemDeleted: EventEmitter<FoodItemI> = new EventEmitter<FoodItemI>();
   @ViewChild(IonItemSliding, { static: false }) slidingItem!: IonItemSliding;
 
-  constructor(private pantryService : PantryApiService, private actionSheetController: ActionSheetController) { }
+  constructor(private pantryService : PantryApiService, private actionSheetController: ActionSheetController, private pickerController: PickerController) { }
 
   ngOnInit() {}
 
@@ -56,6 +56,75 @@ export class FoodListItemComponent  implements OnInit {
      this.pantryService.deletePantryItem(food).subscribe(() => {
       this.itemDeleted.emit(food)
      });
+  }
+
+  async openEditPicker(){
+    const quantityOptions = [];
+    const weightOptions = [];
+
+    let quantitySelectedIndex = 0;
+    let weightSelectedIndex = 0;
+
+    for(let i = 1; i <= 100; i++){
+        quantityOptions.push({
+            text: String(i),
+            value: i
+        });
+
+        weightOptions.push({
+            text: String(i),
+            value: i
+        });
+
+        if(i === this.item.quantity) {
+          quantitySelectedIndex = i - 1;
+        }
+
+        if(i === this.item.weight) {
+            weightSelectedIndex = i - 1;
+        }
+    }
+
+    const picker = await this.pickerController.create({
+      columns: [
+        {
+          name: 'quantity',
+          options: quantityOptions,
+          selectedIndex: quantitySelectedIndex,
+        },
+        {
+          name: 'weight',
+          options: weightOptions,
+          selectedIndex: weightSelectedIndex,
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.closeItem();
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            const updatedItem: FoodItemI = {
+              name: this.item.name,
+              quantity: value.quantity.value,
+              weight: value.weight.value,
+            }
+            this.pantryService.updatePantryItem(updatedItem).subscribe(() => {
+              this.item.quantity = value.quantity.value;
+              this.item.weight = value.weight.value;
+              this.closeItem();
+            });
+          },
+        },
+      ],
+      backdropDismiss: true,
+    });
+    await picker.present();
   }
 
   async editItem(){
