d8948ddf961da20f12dc32c493f1eaabeca03b25
skitsbi <u04525087@tuks.co.za>
2023-06-25 16:35:23 +0200

    added basic controller functions

--- backend/src/main/java/fellowship/mealmaestro/MealmaestroApplication.java
+++ backend/src/main/java/fellowship/mealmaestro/MealmaestroApplication.java
@@ -13,8 +13,6 @@ import fellowship.mealmaestro.services.OpenaiApiService;
 public class MealmaestroApplication {
 
 	public static void main(String[] args) throws JsonProcessingException {
-		MealManagementService ms = new MealManagementService();
-		String res = ms.generateDaysMeals();
 		SpringApplication.run(MealmaestroApplication.class, args);
 	}
 }
--- backend/src/main/java/fellowship/mealmaestro/controllers/MealManagementController.java
+++ backend/src/main/java/fellowship/mealmaestro/controllers/MealManagementController.java
@@ -1,12 +1,27 @@
 package fellowship.mealmaestro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import fellowship.mealmaestro.services.MealManagementService;
 
 @RestController
 public class MealManagementController {
     @Autowired
-    private MealManagementService mealManagementService;
+    private MealManagementService mealManagementService = new MealManagementService();
+
+    @GetMapping("/getDaysMeals")
+    public String dailyMeals() throws JsonMappingException, JsonProcessingException{
+        return mealManagementService.generateDaysMeals();
+    }
+
+    @GetMapping("/getMeal")
+    public String meal() throws JsonMappingException, JsonProcessingException{
+        return mealManagementService.generateMeal();
+    }
+
 }
