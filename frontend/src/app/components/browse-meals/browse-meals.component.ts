8387e9e816b9976ab3fd3ff4d12d96893a781c48
AmickeC <croucampa01@gmail.com>
2023-06-25 22:21:48 +0200

    added card to browse component

--- frontend/src/app/components/browse-meals/browse-meals.component.ts
+++ frontend/src/app/components/browse-meals/browse-meals.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-browse-meals',
   templateUrl: './browse-meals.component.html',
   styleUrls: ['./browse-meals.component.scss'],
+  standalone:true,
+  imports: [CommonModule, IonicModule],
 })
 export class BrowseMealsComponent  implements OnInit {
 
+  @Input() mealsData!: {title: string, description: string, url: string, ingredients:string, instructions:string, cookingTime:string }[];
+  isModalOpen = false;
+  currentObject :any
+  setOpen(isOpen: boolean, o :any) {
+    if(o==null)
+      o = this.currentObject
+    this.isModalOpen = isOpen;
+    this.setCurrent(o)
+  }
   constructor() { }
 
   ngOnInit() {}
 
+  setCurrent(o : any) {
+    this.currentObject = o;
+  }
 }
