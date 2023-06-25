8be7840db933fc7139a5e54520f58421a528c82f
Wian du Toit <wiandtoit@gmail.com>
2023-06-25 11:32:56 +0200

    Recipes in recipe book now load dynamically

--- frontend/src/app/pages/recipe-book/recipe-book.page.ts
+++ frontend/src/app/pages/recipe-book/recipe-book.page.ts
@@ -11,6 +11,12 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class RecipeBookPage implements OnInit {
+  items = [
+    { image: 'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80', title: 'Salmon' },
+    { image: '/assets/img2.jpg', title: 'Stir-fry' },
+    { image: '/assets/img4.jpg', title: 'Pancakes' },
+    { image: '/assets/img3.jpg', title: 'Raspberry Fruit Salad' }
+  ];
 
   constructor() { }
 
