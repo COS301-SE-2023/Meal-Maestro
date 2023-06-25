117431bba8b1b10a65aa36df843144a778cebd77
AmickeC <croucampa01@gmail.com>
2023-06-25 18:06:53 +0200

    browse component made

---
+++ frontend/src/app/components/browse-meals/browse-meals.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-meals',
+  templateUrl: './browse-meals.component.html',
+  styleUrls: ['./browse-meals.component.scss'],
+})
+export class BrowseMealsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
