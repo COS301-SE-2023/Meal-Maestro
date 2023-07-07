9e2980a1427120ba410bb138edae704f591f4178
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-07-07 17:25:43 +0200

    🚧 JwtService methods implemented

--- backend/src/main/java/fellowship/mealmaestro/services/JwtService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/JwtService.java
@@ -1,13 +1,18 @@
 package fellowship.mealmaestro.services;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -19,8 +24,7 @@ public class JwtService {
     private static final String SIGNING_KEY = dotenv.get("JWT_SECRET");
     
     public String extractUserEmail(String token){
-        
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
@@ -40,5 +44,33 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] signingKeyBytes = Decoders.BASE64.decode(SIGNING_KEY);
         return Keys.hmacShaKeyFor(signingKeyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userEmail = extractUserEmail(token);
+        return (userEmail.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
     }
 }
