a22a20d1ec1795c812bb0ee984dabb265966cd59
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-07-07 19:25:49 +0200

    🚧 Authentication WIP

--- backend/src/main/java/fellowship/mealmaestro/config/SecurityConfig.java
+++ backend/src/main/java/fellowship/mealmaestro/config/SecurityConfig.java
@@ -1,11 +1,43 @@
 package fellowship.mealmaestro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider){
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
     
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authReq -> authReq
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        http.authenticationProvider(authenticationProvider);
+        return http.build();
+    }
 }
