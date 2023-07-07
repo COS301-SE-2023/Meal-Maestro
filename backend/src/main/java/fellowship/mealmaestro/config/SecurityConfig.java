e823c38265b0d88574eb1d99b4a8e4e60e8867b8
Skulderlock <78735770+SkulderLock@users.noreply.github.com>
2023-07-07 18:58:27 +0200

    🚧 Authentication Filter Implemented

---
+++ backend/src/main/java/fellowship/mealmaestro/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package fellowship.mealmaestro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    
+}
