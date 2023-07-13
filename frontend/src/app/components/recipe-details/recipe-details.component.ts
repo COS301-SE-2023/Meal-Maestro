e91e44ed908df233786f453316dfb9ddeec66cd0
Wian du Toit <wiandtoit@gmail.com>
2023-07-13 18:33:20 +0200

    Fixed recipe details not being recognised

--- frontend/src/app/components/recipe-details/recipe-details.component.ts
+++ frontend/src/app/components/recipe-details/recipe-details.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
 })
 export class RecipeDetailsComponent  implements OnInit {
   @Input() image!: string;
--- frontend/src/app/pages/recipe-book/recipe-book.page.ts
+++ frontend/src/app/pages/recipe-book/recipe-book.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.component';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.page.html',
   styleUrls: ['./recipe-book.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RecipeItemComponent]
 })
 export class RecipeBookPage implements OnInit {
   items = [
