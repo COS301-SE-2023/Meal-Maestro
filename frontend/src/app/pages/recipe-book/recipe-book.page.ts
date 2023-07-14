3ef2114b2e6bc31e751176f4e95f0d542e9fee3c
Wian du Toit <wiandtoit@gmail.com>
2023-07-14 13:34:11 +0200

    Added interface for recipe item

--- frontend/src/app/pages/recipe-book/recipe-book.page.ts
+++ frontend/src/app/pages/recipe-book/recipe-book.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.component';
+import { RecipeItemI } from '../../models/recipeItem.model';
 
 @Component({
   selector: 'app-recipe-book',
@@ -12,7 +13,7 @@ import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.co
   imports: [IonicModule, CommonModule, FormsModule, RecipeItemComponent]
 })
 export class RecipeBookPage implements OnInit {
-  items = [
+  items: RecipeItemI[] = [
     { image: 'https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80', title: 'Salmon' },
     { image: '/assets/img2.jpg', title: 'Stir-fry' },
     { image: '/assets/img4.jpg', title: 'Pancakes' },
