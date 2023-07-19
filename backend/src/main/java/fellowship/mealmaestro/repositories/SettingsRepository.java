eda66870c88f3fd2c13aea1a49b5eacad5ed7ff9
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-19 23:43:53 +0200

    created the getSettings fuction in Repotitory

--- backend/src/main/java/fellowship/mealmaestro/repositories/SettingsRepository.java
+++ backend/src/main/java/fellowship/mealmaestro/repositories/SettingsRepository.java
@@ -1,15 +1,12 @@
 package fellowship.mealmaestro.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.TransactionCallback;
 import org.neo4j.driver.Values;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
+import org.neo4j.driver.Value;
 import fellowship.mealmaestro.models.SettingsModel;
 
 @Repository
@@ -22,22 +19,56 @@ public class SettingsRepository {
         this.driver = driver;
     }
 
-    public List<SettingsModel> getSettings(String email){
-        try (Session session = driver.session()){
-            return session.executeRead(getSettingsTransaction(email));
-        }
+   public SettingsModel getSettings(String email){
+    try (Session session = driver.session()){
+        return session.executeRead(getSettingsTransaction(email));
     }
+}
 
-    public static TransactionCallback<SettingsModel> addToPantryTransaction(SettingsModel food, String email){
-        return transaction -> {
-            transaction.run("MATCH (User{email: $email})-[:HAS_PANTRY]->(p:Pantry) \r\n" + //
-                        "CREATE (p)-[:IN_PANTRY]->(:Food {name: $name, quantity: $quantity, weight: $weight})",
-    
-            Values.parameters("email", email, "name", food.getName(),
-                        "quantity", food.getQuantity(), "weight", food.getWeight()));
-            SettingsModel addedFood = new SettingsModel(food.getName(), food.getQuantity(), food.getWeight());
-            return addedFood;
-        };
-    }
+public static TransactionCallback<SettingsModel> getSettingsTransaction(String email) {
+    return transaction -> {
+        var result = transaction.run("MATCH (User {email: $email})-[:HAS_PREFERENCES]->(s:Settings) " +
+                "RETURN s.goal AS goal, s.shoppingInterval AS shoppingInterval, s.foodPreferences AS foodPreferences, " +
+                "s.calorieAmount AS calorieAmount, s.budgetRange AS budgetRange, s.macroRatio AS macroRatio, " +
+                "s.allergies AS allergies, s.cookingTime AS cookingTime, s.userHeight AS userHeight, s.userWeight AS userWeight, " +
+                "s.userBMI AS userBMI, s.BMISet AS BMISet, s.cookingTimeSet AS cookingTimeSet, s.allergiesSet AS allergiesSet, " +
+                "s.macroSet AS macroSet, s.budgetSet AS budgetSet, s.calorieSet AS calorieSet, s.foodPreferenceSet AS foodPreferenceSet, " +
+                "s.shoppingIntervalSet AS shoppingIntervalSet",
+                Values.parameters("email", email));
+
+        if (result.hasNext()) {
+            var record = result.next();
+            SettingsModel settings = new SettingsModel(
+                record.get("goal").asString(),
+                record.get("shoppingInterval").asString(),
+                record.get("foodPreferences").asList(Value::asString),
+                record.get("calorieAmount").asInt(),
+                record.get("budgetRange").asString(),
+                record.get("macroRatio").asList(Value::asNumber), //need to convert to map somehow
+                record.get("allergies").asList(Value::asString),
+                record.get("cookingTime").asInt(),
+                record.get("userHeight").asInt(),
+                record.get("userWeight").asInt(),
+                record.get("userBMI").asInt(),
+                record.get("BMISet").asBoolean(),
+                record.get("cookingTimeSet").asBoolean(),
+                record.get("allergiesSet").asBoolean(),
+                record.get("macroSet").asBoolean(),
+                record.get("budgetSet").asBoolean(),
+                record.get("calorieSet").asBoolean(),
+                record.get("foodPreferenceSet").asBoolean(),
+                record.get("shoppingIntervalSet").asBoolean()
+            );
+            return settings;
+        }
+        return null;
+    };
+}
+
+public void updateSettings(SettingsModel request, String email) {
     
 }
+
+}
+
+
