8450761e55c2229264b517fdfcb63a0590901130
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-19 23:43:19 +0200

    cleaned up some code

--- backend/src/main/java/fellowship/mealmaestro/services/SettingsService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/SettingsService.java
@@ -3,12 +3,6 @@ package fellowship.mealmaestro.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import fellowship.mealmaestro.models.SettingsModel;
 import fellowship.mealmaestro.repositories.SettingsRepository;
@@ -18,9 +12,10 @@ import fellowship.mealmaestro.services.auth.JwtService;
 @Service
 public class SettingsService {
 
+
     @Autowired
     private JwtService jwtService;
-
+    
     @Autowired
     private SettingsRepository SettingsRepository;
 
