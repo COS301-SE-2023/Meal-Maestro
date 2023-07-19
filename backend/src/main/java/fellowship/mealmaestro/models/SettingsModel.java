42bdb86bd6d6625f608814333cc407230dfcde46
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-19 10:07:06 +0200

    WIP: added SettingsModel-backend

---
+++ backend/src/main/java/fellowship/mealmaestro/models/SettingsModel.java
@@ -0,0 +1,18 @@
+package fellowship.mealmaestro.models;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import fellowship.mealmaestro.models.auth.AuthorityRoleModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class SettingsModel{
+   
+    
+}
