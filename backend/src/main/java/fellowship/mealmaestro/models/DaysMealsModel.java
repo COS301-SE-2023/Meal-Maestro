89f3457971d2d8298833cf18ff9135b35e28df9c
skitsbi <u04525087@tuks.co.za>
2023-07-21 11:30:21 +0200

    updated meal relationships and added date to identify

--- backend/src/main/java/fellowship/mealmaestro/models/DaysMealsModel.java
+++ backend/src/main/java/fellowship/mealmaestro/models/DaysMealsModel.java
@@ -1,48 +1,67 @@
 package fellowship.mealmaestro.models;
 
+import java.util.Date;
+
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 
 public class DaysMealsModel {
+    @Relationship(type = "breakfast")
     @NotBlank(message = "breakfast meal required")
-    private String breakfast;
+    private MealModel breakfast;
 
+    @Relationship(type = "lunch")
     @NotBlank(message = "lunch meal required")
-    private String lunch;
+    private MealModel lunch;
 
+    @Relationship(type = "dinner")
     @NotBlank(message = "dinner meal required")
-    private String dinner;
+    private MealModel dinner;
 
+    @NotBlank(message = "date required")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date mealDate;
    
 
-    public DaysMealsModel(String breakfast, String lunch,String dinner){
+    public DaysMealsModel(MealModel breakfast, MealModel lunch, MealModel dinner, Date mealDate){
         this.breakfast = breakfast;
         this.lunch = lunch;
         this.dinner = dinner;
-      
+        this.mealDate = mealDate;
     }
 
-    public String getBreakfast(){
+    public MealModel getBreakfast(){
         return this.breakfast;
     }
 
-    public void setBreakfast(String breakfast){
+    public void setBreakfast(MealModel breakfast){
         this.breakfast = breakfast;
     }
 
-    public String getLunch(){
+    public MealModel getLunch(){
         return this.lunch;
     }
 
-    public void setLunch(String lunch){
+    public void setLunch(MealModel lunch){
         this.lunch = lunch;
     }
 
-    public String getDinner(){
+    public MealModel getDinner(){
         return this.dinner;
     }
 
-    public void setDinner(String dinner){
+    public void setDinner(MealModel dinner){
         this.dinner = dinner;
     }
-   
+    
+    public void setMealDate(Date mealDate){
+        this.mealDate = mealDate;
+    }
+
+    public Date getMealDate(){
+        return this.mealDate;
+    }
 }
