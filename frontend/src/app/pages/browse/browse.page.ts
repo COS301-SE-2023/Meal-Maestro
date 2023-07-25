2315b92904734b2b7ffbf29cc1b32bf10702cee3
AmickeC <croucampa01@gmail.com>
2023-07-26 00:06:31 +0200

    testing array

--- frontend/src/app/pages/browse/browse.page.ts
+++ frontend/src/app/pages/browse/browse.page.ts
@@ -34,6 +34,45 @@ export class BrowsePage implements OnInit{
     }
 
  async ngOnInit() {
+  
+    // for (let index = 0; index < 8; index++) {
+    //   this.mealGenerationservice.getPopularMeals().subscribe({
+    //     next: (data) => {
+    //       // Parse the JSON string into a JavaScript array of objects
+    //       let mealsArray: MealBrowseI[] = JSON.parse(data);
+
+    //       // Add the parsed mealsArray to the popularMeals array
+    //       this.popularMeals = this.popularMeals.concat(mealsArray);
+
+    //       console.log(this.popularMeals);
+    //     },
+    //     error: (err) => {
+    //       this.errorHandlerService.presentErrorToast(
+    //         'Error loading meal items', err
+    //       );
+    //     }
+    //   })
+
+    // }
+    // for (let index = 0; index < 8; index++) {
+    //   try {
+    //     const data = await this.mealGenerationservice.getPopularMeals().toPromise();
+    //     // Parse the JSON string into a JavaScript array of objects
+    //     const mealsArray: MealBrowseI[] = JSON.parse(data);
+
+    //     // Add the parsed mealsArray to the popularMeals array
+    //     this.popularMeals = this.popularMeals.concat(mealsArray);
+
+    //     console.log(this.popularMeals);
+    //   } catch (err) {
+    //     this.errorHandlerService.presentErrorToast(
+    //       'Error loading meal items', err
+    //     );
+    //   }
+    // }
+  
+
+
   for (let index = 0; index < 8; index++) {
     this.mealGenerationservice.getPopularMeals().subscribe({
       next: (data) => {
