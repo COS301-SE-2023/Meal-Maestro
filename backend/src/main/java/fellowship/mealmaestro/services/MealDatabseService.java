74780a5828d6436a965c44006f7bdcc03ddbcc29
skitsbi <u04525087@tuks.co.za>
2023-07-26 09:55:41 +0200

    Update MealDatabseService.java

--- backend/src/main/java/fellowship/mealmaestro/services/MealDatabseService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/MealDatabseService.java
@@ -1,8 +1,30 @@
 package fellowship.mealmaestro.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fellowship.mealmaestro.repositories.DaysMealsRepository;
+import fellowship.mealmaestro.repositories.MealRepository;
 
 @Service
 public class MealDatabseService {
+    private final MealRepository mealRepository;
+    public MealRepository getMealRepository() {
+        return mealRepository;
+    }
+
+    private final DaysMealsRepository daysMealsRepository;
+    
+    public DaysMealsRepository getDaysMealsRepository() {
+        return daysMealsRepository;
+    }
+
+    @Autowired
+    public MealDatabseService(MealRepository mealRepository, DaysMealsRepository daysMealsRepository) {
+        this.daysMealsRepository = daysMealsRepository;
+        this.mealRepository = mealRepository;
+
+    }
+
     
 }
