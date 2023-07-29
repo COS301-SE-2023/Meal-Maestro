406b62e2347e04db95b09e2a5972beab2a94282e
AmickeC <croucampa01@gmail.com>
2023-07-29 23:11:12 +0200

    uncommented tests

--- frontend/src/app/pages/browse/browse.page.spec.ts
+++ frontend/src/app/pages/browse/browse.page.spec.ts
@@ -1,33 +1,33 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { BrowsePage } from './browse.page';
-// //import { ExploreContainerComponent } from '../../components/explore-container/explore-container.component';
-// import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowsePage } from './browse.page';
+//import { ExploreContainerComponent } from '../../components/explore-container/explore-container.component';
+import { IonicModule } from '@ionic/angular';
 
-// import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.component';
-// import { MealGenerationService } from '../../services/meal-generation/meal-generation.service';
+import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.component';
+import { MealGenerationService } from '../../services/meal-generation/meal-generation.service';
 
 
-// describe('BrowsePage', () => {
-//   let component: BrowsePage;
-//   let fixture: ComponentFixture<BrowsePage>;
-//   let mockMealGenerationService: jasmine.SpyObj<MealGenerationService>;
+describe('BrowsePage', () => {
+  let component: BrowsePage;
+  let fixture: ComponentFixture<BrowsePage>;
+  let mockMealGenerationService: jasmine.SpyObj<MealGenerationService>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
 
-//       imports: [BrowsePage, IonicModule, RecipeItemComponent],
-//       providers: [
-//         { provide: MealGenerationService, useValue: mockMealGenerationService },
-//       ],
+      imports: [BrowsePage, IonicModule, RecipeItemComponent],
+      providers: [
+        { provide: MealGenerationService, useValue: mockMealGenerationService },
+      ],
 
-//     }).compileComponents();
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(BrowsePage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(BrowsePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
