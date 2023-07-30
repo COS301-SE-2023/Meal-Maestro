b280d81a236292ab59c2d85c6f6d0307f8372525
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-30 21:01:25 +0200

    defaulting macro and cooking

--- frontend/src/app/models/userpreference.model.ts
+++ frontend/src/app/models/userpreference.model.ts
@@ -1,24 +1,24 @@
 export interface UserPreferencesI {
     goal: string;
-    shopping_interval: string;
-    food_preferences: string[];
-    calorie_amount: number;
-    budget_range: string;
-    macro_ratio: {protein: number, carbs: number, fats: number};
+    shoppingInterval: string;
+    foodPreferences: string[];
+    calorieAmount: number;
+    budgetRange: string;
+    macroRatio: {protein: number, carbs: number, fat: number};
     allergies: string[];
-    cooking_time: number;
-    user_height: number; //consider moving to account
-    user_weight: number; //consider moving to account
-    user_BMI: number;
+    cookingTime: number;
+    userHeight: number; //consider moving to account
+    userWeight: number; //consider moving to account
+    userBMI: number;
 
-    BMI_set : boolean;
-    cookingtime_set : boolean;
-    allergies_set : boolean;
-    macro_set : boolean;
-    budget_set : boolean;
-    calorie_set : boolean;
-    foodpreferance_set : boolean;
-    shoppinginterfval_set : boolean;
+    bmiset : boolean;
+    cookingTimeSet : boolean;
+    allergiesSet : boolean;
+    macroSet : boolean;
+    budgetSet : boolean;
+    calorieSet : boolean;
+    foodPreferenceSet : boolean;
+    shoppingIntervalSet : boolean;
 
     
   }
