593fc9850ad04ebcc3f648763b17d74ca9352141
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-31 21:38:55 +0200

    updated test for profile page

--- frontend/src/app/pages/profile/profile.page.spec.ts
+++ frontend/src/app/pages/profile/profile.page.spec.ts
@@ -1,21 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ProfilePage } from './profile.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProfilePage, IonicModule],
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      //removed ProfilePage from declarations
+      imports: [IonicModule.forRoot(), HttpClientTestingModule, RouterTestingModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // added this line
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfilePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
