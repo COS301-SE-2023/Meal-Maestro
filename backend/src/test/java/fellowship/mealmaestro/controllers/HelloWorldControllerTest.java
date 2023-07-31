c2723c941e03a3d535f446e49fdcf661d55342de
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-07-31 11:51:05 +0200

    ✅ Backend tests updated

--- backend/src/test/java/fellowship/mealmaestro/controllers/HelloWorldControllerTest.java
+++ backend/src/test/java/fellowship/mealmaestro/controllers/HelloWorldControllerTest.java
@@ -11,10 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "JWT_SECRET=secret",
+    "DB_URI=bolt://localhost:7687",
+    "DB_USERNAME=neo4j",
+    "DB_PASSWORD=password"
+})
 public class HelloWorldControllerTest {
     
     @Autowired
