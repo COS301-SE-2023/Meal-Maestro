52f87766dfa69eac909aabebac889ba690207fdc
skitsbi <u04525087@tuks.co.za>
2023-07-31 15:41:54 +0200

    Update daysMeals.model.ts

--- frontend/src/app/models/daysMeals.model.ts
+++ frontend/src/app/models/daysMeals.model.ts
@@ -1,8 +1,8 @@
 import { MealI } from "./meal.model";
 
 export interface DaysMealsI {
-    breakfast:MealI;
-    lunch:MealI;
-    dinner:MealI;
+    breakfast:MealI | undefined;
+    lunch:MealI | undefined ;
+    dinner:MealI | undefined;
     mealDate:string | undefined;
 }
