767fee6d3bdf05a2a4b1910bbeba269fc5cc19da
Krygsmancode <frankoswanepoel@gmail.com>
2023-07-31 21:38:16 +0200

    removed redundant imports

--- backend/src/main/java/fellowship/mealmaestro/controllers/SettingsController.java
+++ backend/src/main/java/fellowship/mealmaestro/controllers/SettingsController.java
@@ -2,15 +2,12 @@ package fellowship.mealmaestro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fellowship.mealmaestro.models.SettingsModel;
-import fellowship.mealmaestro.services.PantryService;
 import fellowship.mealmaestro.services.SettingsService;
 import jakarta.validation.Valid;
 
