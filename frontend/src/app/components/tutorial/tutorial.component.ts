0a3c0e5d5451c9ee6655dbe909a4720351844cca
AmickeC <croucampa01@gmail.com>
2023-08-23 20:16:18 +0200

    modal created via component

--- frontend/src/app/components/tutorial/tutorial.component.ts
+++ frontend/src/app/components/tutorial/tutorial.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.component.html',
   styleUrls: ['./tutorial.component.scss'],
+  standalone:true,
+  imports: [IonicModule],
 })
 export class TutorialComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
+
+  closeModal() {
+    this.modalController.dismiss();
+  }
 
   ngOnInit() {}
 
