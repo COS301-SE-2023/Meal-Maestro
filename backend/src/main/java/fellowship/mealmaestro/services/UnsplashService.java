6b63521cb65eaf783730d309fa35d772b7322e2d
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-09-01 18:44:48 +0200

    🚧 Starting with Unsplash API

--- backend/src/main/java/fellowship/mealmaestro/services/UnsplashService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/UnsplashService.java
@@ -1,5 +1,55 @@
 package fellowship.mealmaestro.services;
 
-public class UnsplashService {
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class UnsplashService {
+    private static final String UNSPLASH_URL = "https://api.unsplash.com/search/photos";
+
+    private final static String API_KEY;
+
+    private final WebClient webClient;
+
+    public UnsplashService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    static {
+        String apiKey;
+        Dotenv dotenv;
+        if (System.getenv("UNSPLASH_API_KEY") != null) {
+            apiKey = System.getenv("UNSPLASH_API_KEY");
+        } else {
+            try {
+                dotenv = Dotenv.load();
+                apiKey = dotenv.get("UNSPLASH_API_KEY");
+            } catch (Exception e) {
+                dotenv = Dotenv.configure()
+                        .ignoreIfMissing()
+                        .load();
+                apiKey = "No API Key Found";
+            }
+        }
+        API_KEY = apiKey;
+    }
+
+    public String searchPhotos(String query) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Client-ID " + API_KEY);
+
+        String response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(UNSPLASH_URL)
+                        .queryParam("query", query)
+                        .queryParam("per_page", 1)
+                        .build())
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
+    }
 }
