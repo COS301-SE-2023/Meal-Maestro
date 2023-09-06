915095ac827e51f618de480ebe81b1adea09dea0
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-09-06 20:15:30 +0200

    ✅ Update old tests

--- frontend/src/app/pages/browse/browse.page.spec.ts
+++ frontend/src/app/pages/browse/browse.page.spec.ts
@@ -4,22 +4,26 @@ import { BrowsePage } from './browse.page';
 import { IonicModule } from '@ionic/angular';
 
 import { RecipeItemComponent } from '../../components/recipe-item/recipe-item.component';
-import { MealGenerationService } from '../../services/meal-generation/meal-generation.service';
-
+import {
+  AuthenticationService,
+  MealGenerationService,
+} from '../../services/services';
 
 describe('BrowsePage', () => {
   let component: BrowsePage;
   let fixture: ComponentFixture<BrowsePage>;
   let mockMealGenerationService: jasmine.SpyObj<MealGenerationService>;
+  let mockAuthService: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    mockAuthService = jasmine.createSpyObj('AuthenticationService', ['logout']);
 
+    await TestBed.configureTestingModule({
       imports: [BrowsePage, IonicModule, RecipeItemComponent],
       providers: [
         { provide: MealGenerationService, useValue: mockMealGenerationService },
+        { provide: AuthenticationService, useValue: mockAuthService },
       ],
-
     }).compileComponents();
 
     fixture = TestBed.createComponent(BrowsePage);
--- frontend/src/app/pages/home/home.page.spec.ts
+++ frontend/src/app/pages/home/home.page.spec.ts
@@ -2,20 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { DailyMealsComponent } from '../../components/daily-meals/daily-meals.component';
 import { HomePage } from './home.page';
-import { MealGenerationService } from '../../services/meal-generation/meal-generation.service';
+import {
+  AuthenticationService,
+  MealGenerationService,
+} from '../../services/services';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
   let mockMealGenerationService: jasmine.SpyObj<MealGenerationService>;
+  let mockAuthService: jasmine.SpyObj<AuthenticationService>;
 
   beforeEach(async () => {
     // mockMealGenerationService = jasmine.createSpyObj('MealGenerationService', ['generateMeals']);
+    mockAuthService = jasmine.createSpyObj('AuthenticationService', ['logout']);
 
     await TestBed.configureTestingModule({
       imports: [HomePage, IonicModule, DailyMealsComponent],
       providers: [
         { provide: MealGenerationService, useValue: mockMealGenerationService },
+        { provide: AuthenticationService, useValue: mockAuthService },
       ],
     }).compileComponents();
 
