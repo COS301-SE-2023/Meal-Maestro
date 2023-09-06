df12c1c61dece98c889459ef126093bdf8313283
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-09-06 20:31:04 +0200

    🗃️ New Mongodb models

---
+++ backend/src/main/java/fellowship/mealmaestro/models/mongo/FoodModelM.java
@@ -0,0 +1,40 @@
+package fellowship.mealmaestro.models.mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(collection = "Foods")
+@Getter
+@Setter
+public class FoodModelM {
+    @Id
+    private String barcode;
+
+    private String name;
+
+    private double quantity;
+
+    private String unit;
+
+    private double price;
+
+    public FoodModelM(String barcode, String name, double quantity, String unit, double price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public FoodModelM() {
+    }
+
+    @Override
+    public String toString() {
+        return "FoodModelM [barcode=" + barcode + ", name=" + name + ", price=" + price + ", quantity=" + quantity
+                + ", unit=" + unit + "]";
+    }
+}
