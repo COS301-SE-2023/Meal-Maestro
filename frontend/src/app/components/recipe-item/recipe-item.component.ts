e06c613db8ea65adfcae2e04feca7659ef7e22ea
gryffindor-coder <wiandtoit@gmail.com>
2023-09-15 13:57:05 +0200

    Phase 1 of combining recipe details and item

--- frontend/src/app/components/recipe-item/recipe-item.component.ts
+++ frontend/src/app/components/recipe-item/recipe-item.component.ts
@@ -4,6 +4,7 @@ import { RecipeDetailsComponent } from '../recipe-details/recipe-details.compone
 
 import { MealI } from '../../models/meal.model';
 import { CommonModule } from '@angular/common';
+import { AddRecipeService } from '../../services/recipe-book/add-recipe.service';
 
 
 @Component({
@@ -17,6 +18,10 @@ import { CommonModule } from '@angular/common';
 })
 export class RecipeItemComponent {
   items: MealI[] = [];
+  item!: MealI;
+
+  fIns: string[] = [];
+  fIng: string[] = [];
 
   async openModal(item: any) {
     const modal = await this.modalController.create({
@@ -33,5 +38,37 @@ export class RecipeItemComponent {
     this.items = items;
   }
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private addService: AddRecipeService) { }
+
+  ngOnInit() {
+    if (this.item && this.item.instructions) {
+      this.formatIns(this.item.instructions);
+    }
+
+    if (this.item && this.item.ingredients) {
+      this.formatIng(this.item.ingredients);
+    }
+  }
+
+  private formatIns(ins: string) {
+    const insArr: string[] = ins.split(/\d+\.\s+/);
+    this.fIns = insArr.filter(instruction => instruction.trim() !== '');
+  }
+
+  private formatIng(ing: string) {
+    const ingArr: string[] = ing.split(/,[^()]*?(?![^(]*\))/);
+    this.fIng = ingArr.map((ingredient) => ingredient.trim());
+  }
+
+  closeModal() {
+    this.modalController.dismiss();
+  }
+
+  notSaved(): boolean {
+    return !this.items.includes(this.item);
+  } 
+
+  addRecipe(item: MealI) {
+    this.addService.setRecipeItem(item);
+  }
 }
