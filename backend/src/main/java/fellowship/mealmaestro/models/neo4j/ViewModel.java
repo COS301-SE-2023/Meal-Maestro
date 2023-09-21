79c8ba95ca0d1ce5b87fe22dee56bd308aad9e0a
skitsbi <88lerouxt@gmail.com>
2023-09-21 15:27:22 +0200

    data procesing and normalisation for hashmap

--- backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
+++ backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
@@ -1,6 +1,9 @@
 package fellowship.mealmaestro.models.neo4j;
 
 import java.util.HashMap;
+
+import javax.script.ScriptEngine;
+
 import org.springframework.data.neo4j.core.schema.Node;
 
 import lombok.Data;
@@ -8,31 +11,65 @@ import lombok.Data;
 @Node("View")
 public class ViewModel {
     @Data
-    private class StatValues {
-        public Double Score;
-        public Double minScore;
-        public Double maxScore;
+    private class Scores {
+        public Double score;
+        public Double nScore;
     }
-
-    private HashMap<String,StatValues> ScoreMap = new HashMap<>();
+    private HashMap<String, Scores> ScoreMap = new HashMap<>();
+    private Double max;
+    private Double min;
 
     public ViewModel() {
     }
 
-    public ViewModel(HashMap<String,StatValues> ScoreMap) {
+    public ViewModel(HashMap<String, Scores> ScoreMap) {
         this.ScoreMap = ScoreMap;
     }
 
-    public HashMap<String,StatValues> getScoreMap() {
+    public HashMap<String, Scores> getScoreMap() {
         return this.ScoreMap;
     }
 
-    public void setScoreMap(HashMap<String,StatValues> ScoreMap) {
+    public void setScoreMap(HashMap<String, Scores> ScoreMap) {
         this.ScoreMap = ScoreMap;
     }
-    
-    public void updateScore(String ingredient, Double Score){
 
+    public void updateScore(String ingredient, Double Score) {
+        if (ScoreMap.containsKey(ingredient)) {
+            Boolean changed = false;
+            Scores scores = ScoreMap.get(ingredient);
+            scores.score += Score;
+            if (Score > max) {
+                max = Score;
+                changed = true;
+            }
+
+            if (Score < min) {
+                min = Score;
+                changed = true;
+            }
+            scores.nScore = normalise(scores.score);
+            if(changed){
+                normalise();
+            }
+
+        } else {
+            Scores scores = new Scores();
+            scores.score = Score;
+            scores.nScore = 0.0;
+            ScoreMap.put(ingredient, scores);
+        }
+    }
+
+    public Double normalise(Double Score) {
+        return 2 * ((Score - min) / (max - min)) - 1;
+
+    }
+    public void normalise() {
+       // return 2 * ((Score - min) / (max - min)) - 1;
+       for(Scores scores : ScoreMap.values()){
+            scores.nScore = 2 * ((scores.score - min) / (max - min)) - 1;
+       }
     }
 
 }
