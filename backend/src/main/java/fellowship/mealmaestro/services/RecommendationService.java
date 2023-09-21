aeaefec0dc7d931291b092f32941484246227fa2
skitsbi <88lerouxt@gmail.com>
2023-09-21 16:59:37 +0200

    done for the day i cant think

--- backend/src/main/java/fellowship/mealmaestro/services/RecommendationService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/RecommendationService.java
@@ -1,5 +1,10 @@
 package fellowship.mealmaestro.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fellowship.mealmaestro.repositories.neo4j.UserRepository;
+
 public class RecommendationService {
-    
+    @Autowired
+    private UserRepository userRepository;
 }
