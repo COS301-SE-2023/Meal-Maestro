fa0bedf7cf45cc535b1cec82da50c67bfdd2d75e
skitsbi <88lerouxt@gmail.com>
2023-09-21 13:46:26 +0200

    update view model and efficient processing

--- backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
+++ backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
@@ -3,25 +3,36 @@ package fellowship.mealmaestro.models.neo4j;
 import java.util.HashMap;
 import org.springframework.data.neo4j.core.schema.Node;
 
+import lombok.Data;
+
 @Node("View")
 public class ViewModel {
+    @Data
+    private class StatValues {
+        public Double Score;
+        public Double minScore;
+        public Double maxScore;
+    }
 
-    private HashMap<String,Double> ScoreMap = new HashMap<>();
+    private HashMap<String,StatValues> ScoreMap = new HashMap<>();
 
     public ViewModel() {
     }
 
-    public ViewModel(HashMap<String,Double> ScoreMap) {
+    public ViewModel(HashMap<String,StatValues> ScoreMap) {
         this.ScoreMap = ScoreMap;
     }
 
-    public HashMap<String,Double> getScoreMap() {
+    public HashMap<String,StatValues> getScoreMap() {
         return this.ScoreMap;
     }
 
-    public void setScoreMap(HashMap<String,Double> ScoreMap) {
+    public void setScoreMap(HashMap<String,StatValues> ScoreMap) {
         this.ScoreMap = ScoreMap;
     }
+    
+    public void updateScore(String ingredient, Double Score){
 
-   
+    }
+
 }
--- backend/src/main/java/fellowship/mealmaestro/services/HydrationService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/HydrationService.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import fellowship.mealmaestro.models.neo4j.UserModel;
+import fellowship.mealmaestro.models.neo4j.relationships.HasLogEntry;
 import fellowship.mealmaestro.repositories.neo4j.UserRepository;
 import java.util.List;
 
@@ -16,7 +17,14 @@ public class HydrationService {
     @Scheduled(fixedRate = 20 * 60 * 1000)
     public void pollLogs() {
         List<UserModel> userList = userRepository.findUsersWithNewLogEntries();
-        
+        //per user
+        for(UserModel user : userList){
+            List<HasLogEntry> logEntries = userRepository.findUnprocessedLogEntriesForUser(user);
+            for(HasLogEntry entry : logEntries)
+            {
+                
+            }
+        }
 
     }
    
