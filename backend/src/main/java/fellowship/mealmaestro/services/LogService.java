be387d682fadd48b45c4e263931789409dea5fbe
skitsbi <88lerouxt@gmail.com>
2023-09-21 11:06:20 +0200

    logging of meals added

--- backend/src/main/java/fellowship/mealmaestro/services/LogService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/LogService.java
@@ -1,19 +1,36 @@
 package fellowship.mealmaestro.services;
 
+import java.io.Console;
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Service;
 
 import fellowship.mealmaestro.models.neo4j.MealModel;
 import fellowship.mealmaestro.models.neo4j.UserModel;
+import fellowship.mealmaestro.models.neo4j.relationships.HasLogEntry;
+import fellowship.mealmaestro.repositories.neo4j.UserRepository;
 
 @Service
 public class LogService {
     @Autowired
     private UserService userService;
+    @Autowired
+    private MealDatabaseService mealDatabaseService;
+    @Autowired 
+    private UserRepository userRepository;
 
     public void logMeal(String token, MealModel meal, String entryType){
         UserModel user = userService.getUser(token);
-        
+        Optional<MealModel> dbMeal = mealDatabaseService.findMealForUser(meal.getName(), token);
+        if(dbMeal.isPresent())
+            {
+                HasLogEntry entry = new HasLogEntry(dbMeal.get(),LocalDate.now(), entryType);
+                user.getEntries().add(entry);
+                userRepository.save(user);
+                System.out.println("LogEntry saved! ("+ meal.getName() +","+ entryType +")");
+            }
     }
 }
