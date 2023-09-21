8a7233262510fdde0b53d1f22242b4e08e90f2d0
skitsbi <88lerouxt@gmail.com>
2023-09-21 16:51:26 +0200

    View Creation

--- backend/src/main/java/fellowship/mealmaestro/services/HydrationService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/HydrationService.java
@@ -8,6 +8,8 @@ import fellowship.mealmaestro.models.neo4j.UserModel;
 import fellowship.mealmaestro.models.neo4j.ViewModel;
 import fellowship.mealmaestro.models.neo4j.relationships.HasLogEntry;
 import fellowship.mealmaestro.repositories.neo4j.UserRepository;
+
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -18,32 +20,70 @@ public class HydrationService {
     @Scheduled(fixedRate = 20 * 60 * 1000)
     public void pollLogs() {
         List<UserModel> userList = userRepository.findUsersWithNewLogEntries();
-        //per user
-        for(UserModel user : userList){
+        // per user
+        for (UserModel user : userList) {
             List<HasLogEntry> logEntries = userRepository.findUnprocessedLogEntriesForUser(user);
             ViewModel viewModel = user.getView();
 
-            for(HasLogEntry entry : logEntries)
-            {
+            for (HasLogEntry entry : logEntries) {
+
                 String ingredientString = entry.getMeal().getIngredients();
-                //trim ingredient list
+                // trim ingredient list
+                ingredientString = trimCharacters(ingredientString);
+                // convert to List<String>
+                List<String> ingredientList = parseCommaSeparatedString(ingredientString);
+                // Scores ++ * multiplier
+                Double S_MULTIPLIER = getScoreValue(entry.getEntryType());
 
-                //convert to List<String> 
+                // update view model
+                for (String ingredient : ingredientList) {
+                    viewModel.updateScore(ingredient, S_MULTIPLIER);
+                }
 
-                //Scores ++ * multiplier
-
-                //update view model
-
-                //set processed
+                // set processed
                 entry.setProcessed(true);
             }
+
+            user.setView(viewModel);
+            userRepository.save(user);
         }
 
     }
+
     // helper functions to be done
-    //trim
+    // trim
+    private static String trimCharacters(String input) {
+        String regex = "[0-9\\s]+";
+        String result = input.replaceAll(regex, "");
+        return result;
+    }
 
-    //convert
+    // convert
+    private static List<String> parseCommaSeparatedString(String input) {
+        String[] elements = input.split(",");
+        List<String> result = Arrays.asList(elements);
 
-    //scores
+        return result;
+    }
+
+    // scores
+    private static Double getScoreValue(String entryType) {
+        switch (entryType.toLowerCase()) {
+            case "regenerated":
+                return -0.9;
+                
+            case "like":
+                return 1.0;
+
+            case "dislike":
+                return -0.5;
+
+            case "save":
+                return 0.7;
+
+            default:
+                return 0.5;
+
+        }
+    }
 }
