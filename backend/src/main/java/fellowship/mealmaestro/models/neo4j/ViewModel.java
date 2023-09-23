0e6d09e32847ab1df73900fdddb4d0f13b3e5595
skitsbi <88lerouxt@gmail.com>
2023-09-23 14:50:27 +0200

    Update ViewModel.java

--- backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
+++ backend/src/main/java/fellowship/mealmaestro/models/neo4j/ViewModel.java
@@ -9,8 +9,6 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
-import kotlin.coroutines.ContinuationInterceptor.Key;
-
 @Node("View")
 public class ViewModel {
 
