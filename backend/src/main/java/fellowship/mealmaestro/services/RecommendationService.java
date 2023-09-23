7762403d7726db7c12af6dd1d278316ed5536ed1
skitsbi <88lerouxt@gmail.com>
2023-09-23 15:05:23 +0200

    Common items Function added

--- backend/src/main/java/fellowship/mealmaestro/services/RecommendationService.java
+++ backend/src/main/java/fellowship/mealmaestro/services/RecommendationService.java
@@ -1,5 +1,6 @@
 package fellowship.mealmaestro.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,20 +21,42 @@ public class RecommendationService {
     @Autowired
     private MealManagementService mealManagementService;
     
+    private final Double MIN_VALUE = -0.01;
+
     public MealModel getRecommendedMeal(String token) throws Exception{
         MealModel recMealModel = new MealModel();
         //get view and pantry
-        List<FoodModel> pantryModel = pantryService.getPantry(token.substring(7));
-        ViewModel viewModel = userService.getUser(token).getView();
-        //get positive keys
-           List<String> validIngredients = viewModel.getPositiveNScores(-0.1); 
-        
+        List<String> pantryItems = userService.getUser(token).getPantry().getNameList();
+        List<String> validIngredients = userService.getUser(token).getView().getPositiveNScores(MIN_VALUE); 
+       
         // compare view and pantry
 
+
         //use list to find db meal
+
 
         //query gpt
 
         return recMealModel;
+    }
+
+    public static List<String> findCommonItems(List<String> list1, List<String> list2) {
+        List<String> commonItems = new ArrayList<>();
+        
+        for (String item1 : list1) {
+            for (String item2 : list2) {
+                if (item2.contains(item1)) {
+                    commonItems.add(item1);
+                    break; 
+                }
+                if (item1.contains(item2)) {
+                    commonItems.add(item2);
+                    break; 
+                }
+            }
+        }
+
+        return commonItems;
+
     }
 }
