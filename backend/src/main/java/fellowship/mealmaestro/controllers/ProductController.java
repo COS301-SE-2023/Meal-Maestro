3cb8cb57be557fa906c94ceafad2dc508ef5ba89
Krygsmancode <frankoswanepoel@gmail.com>
2023-09-24 18:13:16 +0200

    pull prep

--- backend/src/main/java/fellowship/mealmaestro/controllers/ProductController.java
+++ backend/src/main/java/fellowship/mealmaestro/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package fellowship.mealmaestro.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,13 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import fellowship.mealmaestro.models.mongo.findBarcodeRequest;
 import fellowship.mealmaestro.models.mongo.FoodModelM;
 import fellowship.mealmaestro.services.BarcodeService;
- import fellowship.mealmaestro.services.webscraping.CheckersScraper;
 
 @RestController
 public class ProductController {
 
     private final BarcodeService barcodeService;
-     private final CheckersScraper checkersScraper;
 
     public ProductController(BarcodeService barcodeService) {
         this.barcodeService = barcodeService;
@@ -38,11 +35,5 @@ public class ProductController {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(barcodeService.addProduct(product));
-    }
-
-    @GetMapping("/loc")
-    public ResponseEntity<String> loc() {
-         checkersScraper.getLocLinks();
-        return ResponseEntity.ok("loc");
     }
 }
