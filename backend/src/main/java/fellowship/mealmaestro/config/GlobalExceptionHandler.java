2963c84b23cb7ace64f460aeab3d80f4db2c07c5
skitsbi <88lerouxt@gmail.com>
2023-09-24 10:10:22 +0200

    cleanup

--- backend/src/main/java/fellowship/mealmaestro/config/GlobalExceptionHandler.java
+++ backend/src/main/java/fellowship/mealmaestro/config/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package fellowship.mealmaestro.config;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
