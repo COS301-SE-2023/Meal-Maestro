5a2ca20a1dffc446395940613aa7c944cf7acdb2
SkulderLock <78735770+SkulderLock@users.noreply.github.com>
2023-09-27 21:12:47 +0200

    ✅ add acc-profile tests

---
+++ frontend/src/app/pages/acc-profile/acc-profile.page.integration.spec.ts
@@ -0,0 +1,87 @@
+import { TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { AccProfilePage } from './acc-profile.page';
+import {
+  AuthenticationService,
+  ErrorHandlerService,
+} from '../../services/services';
+import { UserApiService } from '../../services/user-api/user-api.service';
+import { UserI } from '../../models/user.model';
+
+describe('AccProfilePageIntegration', () => {
+  let component: AccProfilePage;
+  let httpMock: HttpTestingController;
+  let authService: AuthenticationService;
+  let userApiService: UserApiService;
+  let errorHandlerService: ErrorHandlerService;
+  let user: UserI;
+  let apiUrl: string = 'http://68.183.42.105:8080';
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), HttpClientTestingModule],
+      providers: [AuthenticationService, UserApiService, ErrorHandlerService],
+    }).compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+    authService = TestBed.inject(AuthenticationService);
+    userApiService = TestBed.inject(UserApiService);
+    errorHandlerService = TestBed.inject(ErrorHandlerService);
+    component = TestBed.createComponent(AccProfilePage).componentInstance;
+
+    user = {
+      username: 'test',
+      email: 'test@test.com',
+      password: 'test',
+      //   name: 'test',
+    };
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call getUserInfo on ionViewWillEnter', async () => {
+    spyOn(component, 'getUserInfo');
+    component.ionViewWillEnter();
+    expect(component.getUserInfo).toHaveBeenCalled();
+  });
+
+  it('should fetch user Info on getUserInfo', async () => {
+    spyOn(authService, 'getUser').and.callThrough();
+    await component.getUserInfo();
+
+    const req = httpMock.expectOne(apiUrl + '/getUser');
+    expect(req.request.method).toBe('GET');
+    req.flush(user);
+
+    component.user = user;
+
+    expect(authService.getUser).toHaveBeenCalled();
+    expect(component.user).toEqual(user);
+  });
+
+  it('should handle 403 error when getting info', async () => {
+    spyOn(authService, 'getUser').and.callThrough();
+    spyOn(errorHandlerService, 'presentErrorToast').and.callThrough();
+    await component.getUserInfo();
+
+    const req = httpMock.expectOne(apiUrl + '/getUser');
+    expect(req.request.method).toBe('GET');
+    req.flush('403 error', {
+      status: 403,
+      statusText: 'Forbidden',
+    });
+
+    expect(authService.getUser).toHaveBeenCalled();
+    expect(errorHandlerService.presentErrorToast).toHaveBeenCalled();
+  });
+});
--- frontend/src/app/pages/acc-profile/acc-profile.page.spec.ts
+++ frontend/src/app/pages/acc-profile/acc-profile.page.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AccProfilePage } from './acc-profile.page';
-import { IonicModule } from '@ionic/angular';
-import { AuthenticationService } from '../../services/services';
+import { IonicModule, ModalController } from '@ionic/angular';
+import {
+  AuthenticationService,
+  ErrorHandlerService,
+} from '../../services/services';
 import { HttpResponse } from '@angular/common/http';
 import { UserI } from '../../models/user.model';
 import { of } from 'rxjs';
@@ -10,18 +13,28 @@ import { UserApiService } from '../../services/user-api/user-api.service';
 describe('AccProfilePage', () => {
   let component: AccProfilePage;
   let fixture: ComponentFixture<AccProfilePage>;
-  let mockAuthService: jasmine.SpyObj<AuthenticationService>;
-  let mockUserApiService: jasmine.SpyObj<UserApiService>;
+  let authSpy: jasmine.SpyObj<AuthenticationService>;
+  let userApiSpy: jasmine.SpyObj<UserApiService>;
+  let errorHandlerSpy: jasmine.SpyObj<ErrorHandlerService>;
+  let modalControllerSpy: jasmine.SpyObj<ModalController>;
   let mockUser: UserI;
 
   beforeEach(async () => {
-    mockAuthService = jasmine.createSpyObj('AuthenticationService', [
+    authSpy = jasmine.createSpyObj('AuthenticationService', [
       'logout',
       'getUser',
     ]);
 
-    mockUserApiService = jasmine.createSpyObj('UserApiService', [
-      'updateUsername',
+    userApiSpy = jasmine.createSpyObj('UserApiService', ['updateUsername']);
+
+    errorHandlerSpy = jasmine.createSpyObj('ErrorHandlerService', [
+      'presentErrorToast',
+      'presentSuccessToast',
+    ]);
+
+    modalControllerSpy = jasmine.createSpyObj('ModalController', [
+      'create',
+      'dismiss',
     ]);
 
     mockUser = {
@@ -31,13 +44,15 @@ describe('AccProfilePage', () => {
     };
 
     const response = new HttpResponse<UserI>({ body: mockUser, status: 200 });
-    mockAuthService.getUser.and.returnValue(of(response));
+    authSpy.getUser.and.returnValue(of(response));
 
     await TestBed.configureTestingModule({
       imports: [AccProfilePage, IonicModule],
       providers: [
-        { provide: AuthenticationService, useValue: mockAuthService },
-        { provide: UserApiService, useValue: mockUserApiService },
+        { provide: AuthenticationService, useValue: authSpy },
+        { provide: UserApiService, useValue: userApiSpy },
+        { provide: ErrorHandlerService, useValue: errorHandlerSpy },
+        { provide: ModalController, useValue: modalControllerSpy },
       ],
     }).compileComponents();
     fixture = TestBed.createComponent(AccProfilePage);
@@ -47,5 +62,11 @@ describe('AccProfilePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getUserInfo() on ionViewWillEnter()', () => {
+    spyOn(component, 'getUserInfo');
+    component.ionViewWillEnter();
+    expect(component.getUserInfo).toHaveBeenCalled();
   });
 });
